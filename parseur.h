731c87d7d8a2ec4747accd0e4d00541b04fa62fc
Ruimy Benjamin Mac <ruimyb@gmail.com>
2016-05-14 16:27:08 +0200

    Presque bon !!!

--- parseur.h
+++ parseur.h
@@ -5,4 +5,17 @@
 #ifndef TP3_FOUILLE_PARSEUR_H
 #define TP3_FOUILLE_PARSEUR_H
 
+typedef struct {
+    int value;
+    int nbrAppearance;
+}Word;
+
+typedef struct{
+    int categorie;
+    Word *listWord;
+}Document;
+
+
+
+
 #endif //TP3_FOUILLE_PARSEUR_H
