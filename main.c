8ec99c237706459caaec967fc5df4f7e8e255331
Ruimy Benjamin Mac <ruimyb@gmail.com>
2016-05-16 23:40:47 +0200

    "Multinomial Algorithm"

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include "parseur.h"
 #include "testSet.h"
 #include "trainingSet.h"
+#include "multinomial.h"
 #include <stdio.h>
 
 int main(){
@@ -11,25 +12,42 @@ int main(){
     printf("Creation oK\n");
     //afficherAll(L);
 
-    afficherQ1(L);
+    //afficherQ1(L);
 
     // ======== Question 2 : Découpage de l'ensemble de données ========
     tabDoc* test = createTestSet(L);
     //afficherAll(test);
 
     tabDoc* training = createTrainingSet(L);
+
     //afficherAll(training);
+
 
     // ======== Question 3a : estimation des paramètres du modèle de Bernoulli ========
 
 
     // ======== Question 3b : estimation des paramètres du modèle de Multinomial ========
 
+    double **PC;
+    double *Pi;
+    multinomialApprentissage(test,&PC,&Pi);
+    int compt = 0;
+    double res;
+    for(int i = 0; i < 52500; i++){
+        int a = multinomialTest(PC,Pi,L->tab[3]);
+        if (a == training->tab[i]->categorie){
+            compt++;
+        }
+    }
+    res = compt/52500;
+    printf("Pourcentage OK : %lf \n", res);
+
+
     // ======== Question 4 : ========
 
     // ======== Question 5 : ========
 
-    supprimerDoc(L);
+    //supprimerDoc(L);
     printf("Suppression ok\n");
 
     return 0;
